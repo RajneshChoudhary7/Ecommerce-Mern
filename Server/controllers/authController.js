544080a4f904014fbc51f90507b24f5596ff9ae7
Rajneshking <ahirwarrajnesh493@gmail.com>
2025-12-23 15:25:28 +0530

    f

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -24,12 +24,31 @@ export const register = catchAsyncErrors(async(req,res,next)=>{
 
     sendToken(user.rows[0],201,"User registered Successfully",res)
 })
+
+
 export const login = catchAsyncErrors(async(req,res,next)=>{
+    const {email,password} = req.body
+    if(!email||!password){
+        return next(new ErrorHandler("please provide email and password",400))
+    }
+    const user = await database.query(`SELECT * FROM users WHERE email = $1`[
+        email
+    ])
+    if(user.rows.length === 0){
+        return next(new ErrorHandler("Invalid email or password. ",401))
+    }
+    const isPasswordMatch = await bcrypt.compare(password,user.rows[0].password)
+    if(!isPasswordMatch) {
+        return next(new ErrorHandler ("Invalid email or password. " ,401))
+    }
+    sendToken(user.rows[0],200,"logged In.", res);
 
 })
+
+
 export const getUser = catchAsyncErrors(async(req,res,next)=>{
 
 })
 export const logout = catchAsyncErrors(async(req,res,next)=>{
 
-})
+}) 
